ae2e0ecc154fbd2a5e85fac067de792ace2c9e38
laureltremblay <97709177+laureltremblay@users.noreply.github.com>
2025-03-25 17:13:04 -0400

    [feat] basic ui + styling

--- src/callbacks/main_page_callbacks.py
+++ src/callbacks/main_page_callbacks.py
@@ -6,6 +6,8 @@ from graphs.scatter import get_scatter_figure
 def register_main_page_callbacks(app, data_df):
     @app.callback(
         Output('main-graph', 'figure'),
+        Output('heatmap-button', 'className'),
+        Output('scatter-button', 'className'),
         [Input('scatter-button', 'n_clicks'),
          Input('heatmap-button', 'n_clicks')]
     )
@@ -14,6 +16,6 @@ def register_main_page_callbacks(app, data_df):
             return {
                 'data': [],
                 'layout': {}
-            }
+            } , 'header-button heatmap-button active', 'header-button scatter-button'
         else:
-            return get_scatter_figure(data_df)
+            return get_scatter_figure(data_df), 'header-button heatmap-button', 'header-button scatter-button active'
--- src/pages/main_page.py
+++ src/pages/main_page.py
@@ -1,43 +1,42 @@
 from dash import html, dcc
-
 from graphs.scatter import get_scatter_figure
 
 def get_main_page_content(data_df) -> html.Div:
     return html.Div(className='main-page', children=[
         html.Div(className='graph-container', children=[
             html.Div(className='graph-controls', children=[
-                html.Button("Scatter Plot", className="graph-button active", id="scatter-button"),
-                html.Button("Heatmap", className="graph-button", id="heatmap-button")
+                html.Button("Scatter Plot", className="header-button scatter-button active", id="scatter-button"),
+                html.Button("Heatmap", className="header-button heatmap-button", id="heatmap-button")
             ]),
-            html.Div(children=[
-            html.Div(className='graph', id='main-graph-div', children=[
-                dcc.Graph(figure=get_scatter_figure(data_df), id='main-graph', config=dict(
-                    scrollZoom=False,
-                    showTips=False,
-                    showAxisDragHandles=False,
-                    doubleClick=False,
-                    displayModeBar=False
-                ))
-            ]),
-            html.Div(id='filter-container', children=[
-                html.Div(className='filter', children=[
-                    html.Label("Player"),
-                    dcc.Dropdown(id='player-filter', multi=True, placeholder='Select player')
+            html.Div(className='graph-and-filters', children=[
+                html.Div(className='graph', id='main-graph-div', children=[
+                    dcc.Graph(figure=get_scatter_figure(data_df), id='main-graph', config=dict(
+                        scrollZoom=False,
+                        showTips=False,
+                        showAxisDragHandles=False,
+                        doubleClick=False,
+                        displayModeBar=False
+                    ))
                 ]),
-                html.Div(className='filter', children=[
-                    html.Label("Team"),
-                    dcc.Dropdown(id='team-filter', multi=True, placeholder='Select team')
-                ]),
-                html.Div(className='filter', children=[
-                    html.Label("Shot Type"),
-                    dcc.Dropdown(id='shot-type-filter', multi=True, placeholder='Select shot type')
-                ]),
-                html.Div(className='filter', children=[
-                    html.Label("Season"),
-                    dcc.Dropdown(id='season-filter', multi=True, placeholder='Select season')
-                ]),
-            ]
-                )
+                html.Div(id='filter-container', children=[
+                    html.H3("Filters", className="filter-title"),
+                    html.Div(className='filter', children=[
+                        html.Label("Player"),
+                        dcc.Dropdown(id='player-filter', multi=True, placeholder='Select player')
+                    ]),
+                    html.Div(className='filter', children=[
+                        html.Label("Team"),
+                        dcc.Dropdown(id='team-filter', multi=True, placeholder='Select team')
+                    ]),
+                    html.Div(className='filter', children=[
+                        html.Label("Shot Type"),
+                        dcc.Dropdown(id='shot-type-filter', multi=True, placeholder='Select shot type')
+                    ]),
+                    html.Div(className='filter', children=[
+                        html.Label("Season"),
+                        dcc.Dropdown(id='season-filter', multi=True, placeholder='Select season')
+                    ]),
+                ])
             ])
         ]),
     ])
