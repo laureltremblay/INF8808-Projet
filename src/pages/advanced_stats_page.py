23d36eefb20954570ccceccaf36669dc6b953916
laureltremblay <97709177+laureltremblay@users.noreply.github.com>
2025-04-09 12:11:44 -0400

    [fix] LOI 101 TBK

--- src/pages/advanced_stats_page.py
+++ src/pages/advanced_stats_page.py
@@ -5,57 +5,77 @@ from graphs.scatter_plot_pictogram import get_scatter_plot_pictogram_figure
 from graphs.pie_charts import get_pie_chart_figure
 from components.filter import get_filter_pie_charts
 
-def get_advanced_content(data_df):
-    return html.Div([
-        html.H2("Advanced Statistics Page"),
-        html.P("Here you can add advanced statistics or charts."),
-        html.Div(className='graph', id='q5-stacked-bar-chart-graph-div', children=[
-                    dcc.Graph(figure=get_stacked_bar_chart_figure(data_df), id='q5-stacked-bar-chart-graph', config=dict(
-                        scrollZoom=False,
-                        showTips=False,
-                        showAxisDragHandles=False,
-                        doubleClick=False,
-                        displayModeBar=False
-                    ))
-                ]),
-                
-        html.Div(className='graph', id='q5-bar-chart-graph-div', children=[
-                    dcc.Graph(figure=get_bar_chart_figure(data_df), id='q5-bar-chart-graph', config=dict(
-                        scrollZoom=False,
-                        showTips=False,
-                        showAxisDragHandles=False,
-                        doubleClick=False,
-                        displayModeBar=False
-                    ))
-                ]),
-         html.Div(className = 'graph', id = 'scatter-plot-pictogram-div',
-                  style= {'display': 'flex', 'justifyContent': 'center', 'alignItems': 'center'}, 
-                  children = [
-                    dcc.Graph(figure = get_scatter_plot_pictogram_figure(data_df), id = 'scatter-plot-pictogram-graph', 
-                        config = dict(
-                        scrollZoom = False,
-                        showTips = False,
-                        showAxisDragHandles = False,
-                        doubleClick = False,
-                        displayModeBar = False
-                    ))
-                ]),
 
-        # Conteneur Flex pour ( Filtre | 3 Pie Charts )
-        html.Div(
-            className='pie-charts-and-filter-container',
-            children=[
-                # Colonne de droite :
-                html.Div(
-                    get_pie_chart_figure(data_df),
-                    className='pie-charts-div'
-                ),
-                # Colonne de droite :
-                                html.Div(
-                    get_filter_pie_charts(),
-                    className='pie-filter-div'
-                )
-            ]
-        ),
-])
- 
+def get_advanced_content(data_df):
+    return html.Div(
+        [
+            html.H2("Advanced Statistics Page"),
+            html.P("Here you can add advanced statistics or charts."),
+            html.Div(
+                className="graph",
+                id="q5-stacked-bar-chart-graph-div",
+                children=[
+                    dcc.Graph(
+                        figure=get_stacked_bar_chart_figure(data_df),
+                        id="q5-stacked-bar-chart-graph",
+                        config=dict(
+                            scrollZoom=False,
+                            showTips=False,
+                            showAxisDragHandles=False,
+                            doubleClick=False,
+                            displayModeBar=False,
+                        ),
+                    )
+                ],
+            ),
+            html.Div(
+                className="graph",
+                id="q5-bar-chart-graph-div",
+                children=[
+                    dcc.Graph(
+                        figure=get_bar_chart_figure(data_df),
+                        id="q5-bar-chart-graph",
+                        config=dict(
+                            scrollZoom=False,
+                            showTips=False,
+                            showAxisDragHandles=False,
+                            doubleClick=False,
+                            displayModeBar=False,
+                        ),
+                    )
+                ],
+            ),
+            html.Div(
+                className="graph",
+                id="scatter-plot-pictogram-div",
+                style={
+                    "display": "flex",
+                    "justifyContent": "center",
+                    "alignItems": "center",
+                },
+                children=[
+                    dcc.Graph(
+                        figure=get_scatter_plot_pictogram_figure(data_df),
+                        id="scatter-plot-pictogram-graph",
+                        config=dict(
+                            scrollZoom=False,
+                            showTips=False,
+                            showAxisDragHandles=False,
+                            doubleClick=False,
+                            displayModeBar=False,
+                        ),
+                    )
+                ],
+            ),
+            # Conteneur Flex pour ( Filtre | 3 Pie Charts )
+            html.Div(
+                className="pie-charts-and-filter-container",
+                children=[
+                    # Colonne de droite :
+                    html.Div(get_pie_chart_figure(data_df), className="pie-charts-div"),
+                    # Colonne de droite :
+                    html.Div(get_filter_pie_charts(), className="pie-filter-div"),
+                ],
+            ),
+        ]
+    )
--- src/pages/main_page.py
+++ src/pages/main_page.py
@@ -2,29 +2,54 @@ from dash import html, dcc
 from components.filter import get_filter_container
 from graphs.scatter import get_scatter_figure
 
+
 def get_main_page_content(data_df) -> html.Div:
-    return html.Div(className='main-page', children=[
-        html.Div(className='graph-container', children=[
-            html.Div(className='graph-controls', children=[
-                html.Button("Scatter Plot", className="header-button scatter-button active", id="scatter-button"),
-                html.Button("Heatmap", className="header-button heatmap-button", id="heatmap-button")
-            ]),
-            html.Div(className='graph-and-filters', children=[
-                html.Div(className='graph', id='main-graph-div', children=[
-                    dcc.Graph(
-                        figure=get_scatter_figure(data_df),
-                        id='main-graph',
-                        config=dict(
-                            scrollZoom=False,
-                            showTips=False,
-                            showAxisDragHandles=False,
-                            doubleClick=False,
-                            displayModeBar=False
-                        ),
-                        style={"height": "80vh"}  
-                    )
-                ]),
-                get_filter_container()
-            ])
-        ]),
-    ])
+    return html.Div(
+        className="main-page",
+        children=[
+            html.Div(
+                className="graph-container",
+                children=[
+                    html.Div(
+                        className="graph-controls",
+                        children=[
+                            html.Button(
+                                "Scatter Plot",
+                                className="header-button scatter-button active",
+                                id="scatter-button",
+                            ),
+                            html.Button(
+                                "Heatmap",
+                                className="header-button heatmap-button",
+                                id="heatmap-button",
+                            ),
+                        ],
+                    ),
+                    html.Div(
+                        className="graph-and-filters",
+                        children=[
+                            html.Div(
+                                className="graph",
+                                id="main-graph-div",
+                                children=[
+                                    dcc.Graph(
+                                        figure=get_scatter_figure(data_df),
+                                        id="main-graph",
+                                        config=dict(
+                                            scrollZoom=False,
+                                            showTips=False,
+                                            showAxisDragHandles=False,
+                                            doubleClick=False,
+                                            displayModeBar=False,
+                                        ),
+                                        style={"height": "80vh"},
+                                    )
+                                ],
+                            ),
+                            get_filter_container(),
+                        ],
+                    ),
+                ],
+            ),
+        ],
+    )
