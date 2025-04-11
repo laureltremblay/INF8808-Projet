8446fcbf5ed759b1701554457023283ae880dfdb
Selamet53 <selamet.zamanoglu@polymtl.ca>
2025-04-11 00:59:41 -0400

    ajustement du stacked barchart pour avoir 2 modes + le texte affiché en français

--- src/graphs/stacked_bar_chart.py
+++ src/graphs/stacked_bar_chart.py
@@ -10,55 +10,101 @@ MODES = dict(count='Count', percent='Percent')
 MODE_TO_COLUMN = {MODES['count']: 'ShotCount', MODES['percent']: 'ShotPercent'}
 
 event_category_map = {
-    'GIVE': 'Giveaway',
-    'SHOT': 'Shot on Goal',
-    'HIT': 'Hit',
-    'TAKE': 'Takeaway',
-    'MISS': 'Missed Shot',
-    'FAC': 'Faceoff',
-    'BLOCK': 'Blocked Shot',
-    'DELPEN': 'Delayed Penalty',
-    'STOP': 'Stoppage',
-    'CHL': 'Challenge',
-    'GOAL': 'Goal'
+    'GIVE': 'Perte de palet',
+    'SHOT': 'Tir au but',
+    'HIT': 'Mise en échec',
+    'TAKE': 'Récupération',
+    'MISS': 'Tir raté',
+    'FAC': 'Mise au jeu',
+    'BLOCK': 'Tir bloqué',
+    'DELPEN': 'Pénalité différée',
+    'STOP': 'Arrêt de jeu',
+    'CHL': 'Révision vidéo',
+    'GOAL': 'But'
+    # 'GIVE': 'Giveaway',
+    # 'SHOT': 'Shot on Goal',
+    # 'HIT': 'Hit',
+    # 'TAKE': 'Takeaway',
+    # 'MISS': 'Missed Shot',
+    # 'FAC': 'Faceoff',
+    # 'BLOCK': 'Blocked Shot',
+    # 'DELPEN': 'Delayed Penalty',
+    # 'STOP': 'Stoppage',
+    # 'CHL': 'Challenge',
+    # 'GOAL': 'Goal'
 }
 
-def get_stacked_bar_chart_figure(data_df: pd.DataFrame, mode = MODES['count']):
-    df = data_df.copy(deep=True) # Deepcopy to not affect the global dataframe.
+
+event_types_map = {
+    'SHOT': 'Tir cadré',
+    'MISS': 'Tir raté',
+    'GOAL': 'But',
+}
+
+event_types_colors = {
+    'Tir cadré': '#636EFA', 
+    'Tir raté': '#EF553B', 
+    'But': '#00CC96',
+}
+
+def get_stacked_bar_char_template(mode):
+    if mode == 'Count':
+        hover_template = (
+            "<span> %{y} tirs</span><br>"
+        )
+    elif mode == 'Percent':
+        hover_template = (
+            "<span> %{y:.2f}% des tirs</span><br>"
+        )
+    else:
+        hover_template = "Invalid mode"
+
+    return hover_template
+
+def get_stacked_bar_chart_figure(data_df: pd.DataFrame, mode = MODES['percent']):
+    df = data_df.copy(deep=True)
     
     # Prepare the data.
     event_types = ['SHOT', 'MISS', 'GOAL']
     df_filtered = df[df['event'].isin(event_types)]
     df_filtered['lastEventCategory'] = df_filtered['lastEventCategory'].map(event_category_map)
+    df_filtered['event'] = df_filtered['event'].map(event_types_map)
     df_pivot = df_filtered.groupby(['lastEventCategory', 'event']).size().unstack(fill_value=0)
     
     # Calculate the total and success columns.
     df_pivot['Total'] = df_pivot.sum(axis=1)
-    df_pivot['Success'] = (df_pivot['GOAL'] / df_pivot['Total'] * 100).round(2)
     df_pivot = df_pivot.sort_values('Total', ascending=False)
-
-    # Create the bar stacked chart.
-    fig = go.Figure()
-    colors = {'SHOT': '#636EFA', 'MISS': '#EF553B', 'GOAL': '#00CC96'}
     
+    event_types = ['Tir cadré', 'Tir raté', 'But']
+    df_pivot_percent = df_pivot[event_types].div(df_pivot['Total'], axis=0) * 100
+    
+    fig = go.Figure() 
     for event in event_types:
+        # Select the correct data depending on shot type.
+        y_data = df_pivot_percent[event] if mode == MODES['percent'] else df_pivot[event]
+        
         fig.add_trace(go.Bar(
             x=df_pivot.index,
-            y=df_pivot[event],
+            y=y_data,
             name=event,
-            marker_color=colors.get(event, 'gray'),
-            hoverinfo='y+name',
-            textposition='inside'
+            marker_color=event_types_colors[event],
+            hovertemplate=get_stacked_bar_char_template(mode=mode),
         ))
 
     # Customize the plot layout.
+    yaxis_config = {
+        'title': 'Pourcentage des tirs (%)' if mode == MODES['percent'] else 'Nombre de tirs',
+        'range': [0, 100] if mode == MODES['percent'] else None
+    }
+    
     fig.update_layout(
         barmode='stack',
-        title='Répartition des types de tirs par événement précédent',
+        title='Répartition des types de tirs selon l\'événement précédent',
         xaxis_title='Événement précédent',
-        yaxis_title='Nombre total de tirs',
+        yaxis=yaxis_config,
         hovermode='x unified',
-        plot_bgcolor='white',
+        plot_bgcolor='rgba(245, 245, 245, 0.9)',
+        paper_bgcolor='white',
         legend=dict(
             orientation='h',
             yanchor='bottom',
@@ -66,10 +112,12 @@ def get_stacked_bar_chart_figure(data_df: pd.DataFrame, mode = MODES['count']):
             xanchor='right',
             x=1
         ),
-        margin=dict(b=100)  # Marge pour les annotations.
+        margin=dict(b=100, t=40),
+        font=dict(family="Roboto", size=12)
     )
     
-    fig.update_xaxes(tickangle=-45)
+    fig.update_xaxes(tickangle=-45, showgrid=False)
+    fig.update_yaxes(showgrid=True, gridcolor='rgba(200, 200, 200, 0.3)')
     
     return fig
 
