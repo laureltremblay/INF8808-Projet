b4b381e2eca24171c1ef1d756fc506badce2330f
laureltremblay <97709177+laureltremblay@users.noreply.github.com>
2025-04-15 14:57:28 -0400

    addedf loading screen

--- src/app.py
+++ src/app.py
@@ -11,7 +11,6 @@ from preprocess import basic_filtering
 from assets.load_logos import TEAM_LOGOS
 from callbacks.advanced_page_callbacks import register_advanced_page_callbacks
 
-
 app = dash.Dash(__name__)
 app.title = "NHL Shot Data 2023-2024"
 PORT = 8050
@@ -53,10 +52,16 @@ app.layout = html.Div(
                 ),
             ]
         ),
-        html.Main(
-            id="main-page-content",
-            className="chosen-page",
-            children=[get_main_page_content(data_df)],  # Initial content
+        dcc.Loading(
+            id="loading-spinner",
+            type="circle",
+            color="green",
+            fullscreen=False,
+            children=html.Main(
+                id="main-page-content",
+                className="chosen-page",
+                children=[get_main_page_content(data_df)],  # Initial content
+            ),
         ),
     ],
 )
